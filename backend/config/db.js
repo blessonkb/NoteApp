9c803f472eb20a362f946cbde5990cb6f5e1663a
blessonkb <blessonkb1@gmail.com>
2025-12-07 11:30:54 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose= require("mongoose");
+require("dotenv").config();
+const connectdb= ()=>{
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log("MongoDB Connection Error",error);
+        process.exit(1);
+    }
+}
+module.exports= connectdb;
